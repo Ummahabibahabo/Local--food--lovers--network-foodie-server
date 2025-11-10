9d2adb0a3459f1fe14509b41ddc84cc3bfc14856
ummahabibahabo <habiba.enh@gmail.com>
2025-11-10 13:34:14 +0600

    get all foods in the server

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+const uri =
+  "mongodb+srv://foodiedbUser:VG82YsiJI21cSyrN@cluster0.kailwc7.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("foddie_db");
+    const foodsCollection = db.collection("foods");
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    // GET all foods
+
+    app.get("/foods", async (req, res) => {
+      const cursor = foodsCollection.find().sort({ rating: -1 });
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
