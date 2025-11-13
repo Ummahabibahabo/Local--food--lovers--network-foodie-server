29c360882cfba2ba446f1d9401978df4a807e342
ummahabibahabo <habiba.enh@gmail.com>
2025-11-13 20:40:37 +0600

    local food lovers network foodie server final commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
-
+const admin = require("firebase-admin");
+require("dotenv").config();
+const serviceAccount = require("./ServiceKey.json");
 const app = express();
 const port = 3000;
 
@@ -12,8 +14,11 @@ app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-const uri =
-  "mongodb+srv://foodiedbUser:VG82YsiJI21cSyrN@cluster0.kailwc7.mongodb.net/?appName=Cluster0";
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.kailwc7.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -23,14 +28,34 @@ const client = new MongoClient(uri, {
   },
 });
 
+// Middleware to verify Firebase token
+const verifyToken = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader?.startsWith("Bearer ")) {
+    return res.status(401).send({ success: false, message: "Unauthorized" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = await admin.auth().verifyIdToken(token);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(401).send({ success: false, message: "Invalid token" });
+  }
+};
+
 async function run() {
-  await client.connect();
+  // await client.connect();
 
   const db = client.db("foodie_db");
   const foodsCollection = db.collection("foods");
   const reviewsCollection = db.collection("reviews");
   const favoritesCollection = db.collection("favorites");
-  console.log("Connected to MongoDB!");
+  // await client.db("admin").command({ ping: 1 });
+  console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
   // ------------------- FOODS ROUTES -------------------
   app.get("/foods", async (req, res) => {
@@ -83,7 +108,6 @@ async function run() {
 
   // ------------------- REVIEWS ROUTES
 
-  // Get all reviews OR search by food name
   app.get("/reviews", async (req, res) => {
     const searchText = req.query.search || "";
 
@@ -98,14 +122,13 @@ async function run() {
 
     res.send(result);
   });
-  // Get single review by string id
+
   app.get("/reviews/:id", async (req, res) => {
     const id = req.params.id;
     const result = await reviewsCollection.findOne({ _id: id });
     res.send(result);
   });
 
-  // Get reviews by user email
   app.get("/reviews/user/:email", async (req, res) => {
     const email = req.params.email;
     const result = await reviewsCollection
@@ -115,7 +138,6 @@ async function run() {
     res.send(result);
   });
 
-  // Get reviews by food id
   app.get("/reviews/food/:foodId", async (req, res) => {
     const foodId = req.params.foodId;
     const result = await reviewsCollection
@@ -125,16 +147,7 @@ async function run() {
     res.send(result);
   });
 
-  // Add new review
-  // app.post("/reviews", async (req, res) => {
-  //   const review = req.body;
-  //   if (!review._id) review._id = Date.now().toString(); // string ID
-  //   review.date = new Date();
-  //   const result = await reviewsCollection.insertOne(review);
-  //   res.send(result);
-  // });
-
-  app.post("/reviews", async (req, res) => {
+  app.post("/reviews", verifyToken, async (req, res) => {
     const review = {
       ...req.body,
       _id: Date.now().toString(),
@@ -143,8 +156,8 @@ async function run() {
     const result = await reviewsCollection.insertOne(review);
     res.send(result);
   });
-  // Update review by string id
-  app.patch("/reviews/:id", async (req, res) => {
+
+  app.patch("/reviews/:id", verifyToken, async (req, res) => {
     const id = req.params.id;
     const updatedReview = req.body;
 
@@ -156,17 +169,20 @@ async function run() {
     res.send(result);
   });
 
-  // Delete review by string id
   app.delete("/reviews/:id", async (req, res) => {
     const id = req.params.id;
+
     const result = await reviewsCollection.deleteOne({ _id: id });
 
-    res.send(result);
+    if (result.deletedCount > 0) {
+      res.send({ success: true, message: "Review deleted successfully!" });
+    } else {
+      res.send({ success: false, message: "Review not found" });
+    }
   });
 
   // ---------- FAVORITES ---------
 
-  // Get all favorites (admin/general)
   app.get("/favorites", async (req, res) => {
     const result = await favoritesCollection
       .find()
@@ -175,7 +191,6 @@ async function run() {
     res.send(result);
   });
 
-  // Get all favorites of a specific user
   app.get("/favorites/user/:userEmail", async (req, res) => {
     const userEmail = req.params.userEmail;
     const result = await favoritesCollection
@@ -185,9 +200,7 @@ async function run() {
     res.send(result);
   });
 
-  // Add Favorite post
-
-  app.post("/favorites", async (req, res) => {
+  app.post("/favorites", verifyToken, async (req, res) => {
     const { userEmail, reviewId } = req.body;
     if (!userEmail || !reviewId) {
       res.send({ success: false, message: "Missing info" });
@@ -214,7 +227,6 @@ async function run() {
     }
   });
 
-  // Get single favorite by _id
   app.get("/favorites/:id", async (req, res) => {
     const id = req.params.id;
     const result = await favoritesCollection.findOne({ _id: id });
@@ -225,8 +237,7 @@ async function run() {
     }
   });
 
-  // Delete a favorite
-  app.delete("/favorites/:id", async (req, res) => {
+  app.delete("/favorites/:id", verifyToken, async (req, res) => {
     const id = req.params.id;
     const result = await favoritesCollection.deleteOne({ _id: id });
 
